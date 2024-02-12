4740039a6b3412862277271dcc266d4a7cd81695
TR4E <trae.rohleder@outlook.com>
2024-02-13 02:45:38 +1100

    Changes

--- src/main/java/me/trae/clans/Clans.java
+++ src/main/java/me/trae/clans/Clans.java
@@ -1,5 +1,7 @@
 package me.trae.clans;
 
+import me.trae.champions.role.RoleManager;
+import me.trae.champions.skill.SkillManager;
 import me.trae.clans.clan.ClanManager;
 import me.trae.clans.world.WorldManager;
 import me.trae.core.chat.ChatManager;
@@ -12,6 +14,7 @@ import me.trae.core.item.ItemManager;
 import me.trae.core.network.NetworkManager;
 import me.trae.core.player.PlayerManager;
 import me.trae.core.redis.RedisManager;
+import me.trae.core.server.ServerManager;
 import me.trae.core.updater.UpdaterManager;
 import me.trae.framework.shared.utility.enums.ChatColor;
 
@@ -29,8 +32,13 @@ public class Clans extends SpigotPlugin {
         addManager(new NetworkManager(this));
         addManager(new PlayerManager(this));
         addManager(new RedisManager(this));
+        addManager(new ServerManager(this));
         addManager(new UpdaterManager(this));
         addManager(new WorldManager(this));
+
+        // Champions
+        addManager(new RoleManager(this));
+        addManager(new SkillManager(this));
 
         // Clans
         addManager(new ClanManager(this));
