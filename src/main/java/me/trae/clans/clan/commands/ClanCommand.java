3a6e433c2b176b1c64646a2c98e710871a472ee1
TR4E <trae.rohleder@outlook.com>
2024-02-13 19:04:21 +1100

    Changes

--- src/main/java/me/trae/clans/clan/commands/ClanCommand.java
+++ src/main/java/me/trae/clans/clan/commands/ClanCommand.java
@@ -3,6 +3,7 @@ package me.trae.clans.clan.commands;
 import me.trae.clans.clan.ClanManager;
 import me.trae.core.client.Client;
 import me.trae.core.command.types.PlayerCommand;
+import me.trae.core.utility.UtilMessage;
 import me.trae.framework.shared.client.enums.Rank;
 import org.bukkit.entity.Player;
 
@@ -19,5 +20,13 @@ public class ClanCommand extends PlayerCommand<ClanManager> {
 
     @Override
     public void execute(final Player player, final Client client, final String[] args) {
+        if (args.length > 0 && this.isSubCommandByLabel(args[0])) {
+            this.executeSubCommand(player, args);
+            return;
+        }
+
+        if (args.length == 0) {
+            UtilMessage.message(player, "Clans", "You are not in a Clan.");
+        }
     }
 }
